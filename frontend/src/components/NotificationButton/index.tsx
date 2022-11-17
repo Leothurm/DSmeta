fd00a131deaecc2ebe0f8f842db6fd494557ae79
Leothurm <leonardo.thurm@gmail.com>
2022-11-17 14:38:17 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/Notification-icon.svg'
+import icon from '../../assets/img/Notification-icon.svg';
 
-import './styles.css'
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -11,4 +11,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
