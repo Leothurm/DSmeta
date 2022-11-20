b533724b274ac102d0bb4e4588ec0081598fb7aa
Leothurm <leonardo.thurm@gmail.com>
2022-11-20 11:21:23 -0300

    fix notification icon

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import icon from '../../assets/img/Notification-icon.svg';
+import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 
 import './styles.css';
@@ -13,13 +13,13 @@ type props = {
 function handleClick(id:number){
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            toast.info("SMS enviado com sucesso!")
-        })
+            toast.info("SMS enviado com sucesso!");
+        });
 }
 
 function NotificationButton({ saleId }: props) {
     return (
-        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+        <div className="dsmeta-red-btn" onClick={() => {handleClick(saleId)}}>
             <img src={icon} alt="Notificar" />
         </div >
 
